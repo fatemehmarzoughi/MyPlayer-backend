5a469bd82d487135f5fe7100d5a2a925b9cfb5a1
Saba (Fatemeh) Marzoughi <48021528+fatemehmarzoughi@users.noreply.github.com>
2025-06-08 21:55:25 +0330

    Update database.js

--- config/database.js
+++ config/database.js
@@ -54,8 +54,15 @@ module.exports = ({ env }) => {
       },
     },
     postgres: {
-      connectionString: env("DATABASE_URL"),
-      ssl: env.bool("DATABASE_SSL", true),
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', true),
+        schema: env('DATABASE_SCHEMA', 'public'),
+      },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
